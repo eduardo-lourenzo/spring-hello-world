e09356ff99c8c87d2ae7449f6111d381837852a7
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-13 15:37:20 -0300

    feat: create sping-hello-world project

---
+++ src/test/java/br/edu/zup/spring_hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.zup.spring_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
